0f2312808acab1fc6c2b6fdf47a35d98ce572684
yektt <yektaturan.93@gmail.com>
2021-08-10 16:53:40 +0200

    webpack have been installed and set up

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// entry --> output
+
+
+const path = require('path');
+
+// it is a way to expose something to another file
+module.exports = {
+  entry: './src/app.js',
+  // this will be an object
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
