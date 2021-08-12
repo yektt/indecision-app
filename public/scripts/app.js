a9aa9d60f1b1c568bf39050eee004d4d976c6748
yektt <yektaturan.93@gmail.com>
2021-08-12 11:56:22 +0200

    Set up the babel in webpack

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,45 @@
-'use strict';
+"use strict";
 
-var _utils = require('./utils.js');
+var _react = require("react");
 
-var _utils2 = _interopRequireDefault(_utils);
+var _react2 = _interopRequireDefault(_react);
 
-var _person = require('./person.js');
+var _reactDom = require("react-dom");
 
-var _person2 = _interopRequireDefault(_person);
+var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// inside of the curly bracelets, you can import the function
+// // for string validations
+// import validator from "validator";
+var template = _react2.default.createElement(
+  "p",
+  null,
+  "testing testing"
+);
+_reactDom2.default.render(template, document.getElementById('app'));
 
-// you need to use the default method out of curly bracelets
-console.log('hello');
-console.log((0, _utils.square)(4));
-console.log((0, _utils.add)(2, 3));
-console.log((0, _utils2.default)(5, 3));
+// ***** Import examples
 
-console.log((0, _person.isAdult)(15));
-console.log((0, _person.isAdult)(23));
-console.log((0, _person.canDrink)(12));
-console.log((0, _person.canDrink)(41));
+// // inside of the curly bracelets, you can import the function
 
-// you can change the naming of default imported methods.
-// it is 'isSenior' in the person.js
-console.log((0, _person2.default)(41));
-console.log((0, _person2.default)(65));
+// // you need to use the default method out of curly bracelets
+// import subtract,{ square, add } from './utils.js';
+// import isSeniorCitizen, { canDrink, isAdult } from './person.js';
+
+// console.log('hello');
+// console.log(square(4));
+// console.log(add(2,3))
+// console.log(subtract(5,3));
+
+// console.log(isAdult(15));
+// console.log(isAdult(23));
+// console.log(canDrink(12));
+// console.log(canDrink(41));
+
+// // you can change the naming of default imported methods.
+// // it is 'isSenior' in the person.js
+// console.log(isSeniorCitizen(41));
+// console.log(isSeniorCitizen(65));
+
+// ***** import examples finished
--- src/app.js
+++ src/app.js
@@ -1,20 +1,36 @@
-// inside of the curly bracelets, you can import the function
+// // for string validations
+// import validator from "validator";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// you need to use the default method out of curly bracelets
-import subtract,{ square, add } from './utils.js';
-import isSeniorCitizen, { canDrink, isAdult } from './person.js';
+const template = <p>testing testing</p>;
+ReactDOM.render(template, document.getElementById('app'));
 
-console.log('hello');
-console.log(square(4));
-console.log(add(2,3))
-console.log(subtract(5,3));
 
-console.log(isAdult(15));
-console.log(isAdult(23));
-console.log(canDrink(12));
-console.log(canDrink(41));
 
-// you can change the naming of default imported methods.
-// it is 'isSenior' in the person.js
-console.log(isSeniorCitizen(41));
-console.log(isSeniorCitizen(65));
+
+
+// ***** Import examples
+
+// // inside of the curly bracelets, you can import the function
+
+// // you need to use the default method out of curly bracelets
+// import subtract,{ square, add } from './utils.js';
+// import isSeniorCitizen, { canDrink, isAdult } from './person.js';
+
+// console.log('hello');
+// console.log(square(4));
+// console.log(add(2,3))
+// console.log(subtract(5,3));
+
+// console.log(isAdult(15));
+// console.log(isAdult(23));
+// console.log(canDrink(12));
+// console.log(canDrink(41));
+
+// // you can change the naming of default imported methods.
+// // it is 'isSenior' in the person.js
+// console.log(isSeniorCitizen(41));
+// console.log(isSeniorCitizen(65));
+
+// ***** import examples finished
