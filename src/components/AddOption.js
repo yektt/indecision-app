b40076f9213b04ef85a43c035fbebdb5fe917bda
yektt <yektaturan.93@gmail.com>
2021-08-13 15:59:14 +0200

    Add Option method has been moved to its file under components

---
+++ src/components/AddOption.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+// for class definition, export class can be placed at the beginning
+export default class AddOption extends React.Component {
+  // for sending data from child to parent
+  // you should add constructor method and bind the method again
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    
+    this.state = {
+      error: undefined
+    };
+  }
+  // class method
+  // something that is contained inside of the class
+  // other classes do not this method to run
+  handleAddOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    if(error)
+      console.log(error)
+
+    // this.setState(() => {
+    //   return {
+    //     error
+    //   };
+    // });
+
+    this.setState(() => ({ error }));
+
+    // for clearing input:
+    if (!error) {
+      e.target.elements.option.value = "";
+    }
+  }
+  render() {
+    return (
+      <div>
+        { this.state.error && <p>{this.state.error} </p> }
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
