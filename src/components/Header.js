6a6b1c9dd526aeb23d1aa42701f602e85c78cb84
yektt <yektaturan.93@gmail.com>
2021-08-13 16:00:02 +0200

    Header has been moved to its file under components

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+// class Header extends React.Component{
+//   // React.Component requires one method define : 'render'
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.props.title}</h1>
+//         <h2>{this.props.subtitle}</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// converting Header class to the stateless function component
+
+// setting up a default value for props values (class based or function based components)
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2> }
+    </div>
+  );
+};
+
+// this is an object
+// it can be override in other components
+Header.defaultProps = {
+  title: 'Indesicion'
+};
+
+export default Header;
