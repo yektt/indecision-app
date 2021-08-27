54b3b8380c1972dea716d6d0c3479e98159bf7a9
yektt <yektaturan.93@gmail.com>
2021-08-27 16:46:19 +0200

    created _header.scss partial to style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,9 +16,9 @@ import React from "react";
 
 // setting up a default value for props values (class based or function based components)
 const Header = (props) => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2> }
+  <div className="header">
+    <h1 className="header__title">{props.title}</h1>
+    {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2> }
   </div>
 );
 
