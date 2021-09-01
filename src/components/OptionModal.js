ed63116c76864f71cd959ac52f00109d24e733e2
yektt <yektaturan.93@gmail.com>
2021-09-01 17:01:08 +0200

    styled the modal and its elements

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -9,11 +9,13 @@ const OptionModal = (props) => (
       // clicking a place is not on the modal
       onRequestClose = {props.handleClearSelectedOption}
       contentLabel = "Selected Option"
+      closeTimeoutMS = {200}
+      className = "modal"
     >
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
+      <h3 className="modal__title">Selected Option</h3>
+      {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
 
-      <button onClick={props.handleClearSelectedOption}>Okay</button>
+      <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
     </Modal>
 );
 
