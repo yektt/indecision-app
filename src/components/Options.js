f13f7c236be1032ebff5e2953f7e222a19e4c305
yektt <yektaturan.93@gmail.com>
2021-09-01 15:21:09 +0200

    widget header and title has been styled

--- src/components/Options.js
+++ src/components/Options.js
@@ -21,11 +21,14 @@ import Option from "./Option";
 
 const Options = (props) => (
   <div>
-    <button 
-      className="button button--link"
-      onClick={props.handleDeleteOptions}>
-        Remove all
-    </button>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button 
+        className="button button--link"
+        onClick={props.handleDeleteOptions}>
+          Remove all
+      </button>
+    </div>
     
     {props.options.length === 0 && <p>Please add an option to get started!</p>}
     {
